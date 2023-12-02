ad92a1b3300aa9586b579af25b207b24bb6e867a
brrna <bern.varol@gmail.com>
2023-12-02 12:11:42 +0300

    Hatalar giderildi. Uygulama çalışır halde.

--- App.js
+++ App.js
@@ -1,36 +1,45 @@
-import React, { useState } from "react";
-import { SafeAreaView, FlatList } from "react-native";
-import DraggableFlatList from 'react-native-draggable-flatlist'
+import React, { useState } from 'react';
+import { SafeAreaView, View, Text } from 'react-native';
+import DraggableFlatList from 'react-native-draggable-flatlist';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 import Header from "./src/components/header/Header";
 import Card from "./src/components/card/Card";
 import exercise_data from "./src/exercise_data.json"
 
 function App() {
+  const [data, setData] = useState(exercise_data);
 
-    const [data, setData] = useState(exercise_data)
+  const renderItem = ({ item, index, drag, isActive }) => {
+    return (
+      <Card
+        exercise={item}
+        onLongPress={drag}
+      />
+    );
+  };
 
-    const renderExercise = ({ item }) => <Card exercise={item} />
-    const keyExercise = item => item.id.toString()
+  return (
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <SafeAreaView
+        style={{
+          flex: 1,
+          backgroundColor: "blue",
+          flexDirection: "column",
+        }}
+      >
+        <Header />
 
-    return(
-        <SafeAreaView
-            style={{
-                flex: 1,
-                backgroundColor: "blue",
-                flexDirection: "column",
-            }}>
-
-                <Header />
-                
-                <DraggableFlatList
-                    data={data}
-                    renderItem={renderExercise}
-                    keyExtractor={keyExercise}
-                />
-
-        </SafeAreaView>
-    )
+        <DraggableFlatList
+          data={data}
+          renderItem={renderItem}
+          keyExtractor={(item, index) => `draggable-item-${item.id}`}
+          onDragEnd={({ data }) => setData(data)}
+        />
+      </SafeAreaView>
+    </GestureHandlerRootView>
+  );
 }
 
 export default App;
+
