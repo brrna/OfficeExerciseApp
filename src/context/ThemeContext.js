7d89411390deb410b0250c5fb0f31b9a8586b6f4
brrna <bern.varol@gmail.com>
2024-02-01 16:56:23 +0300

    context file created

---
+++ src/context/ThemeContext.js
@@ -0,0 +1,19 @@
+import { createContext, useState} from "react"
+import { darkTheme, lightTheme } from "../values/Colors";
+
+
+export const ThemeContext = createContext();
+
+export const ThemeContextProvider = (props) => {
+
+    const [isDarkTheme, setIsDarkTheme] = useState(false);
+
+    const theme = isDarkTheme ? darkTheme : lightTheme;
+
+    return(
+        <ThemeContext.Provider
+            value={{isDarkTheme, setIsDarkTheme, theme}}>
+                {props.children}
+        </ThemeContext.Provider>
+    )
+}
