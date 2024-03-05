baee1ae93f5b9e9df55d8abff42123e8b712362a
brrna <bern.varol@gmail.com>
2024-03-05 22:09:33 +0300

    BackHandler added

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,23 +1,38 @@
 import { View } from "react-native";
 import MyHeader from "../../components/myHeader/MyHeader";
 import MyList from "../../components/myList/MyList";
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import { ThemeContext } from "../../context/ThemeContext";
+import { BackHandler } from "react-native";
 
 function Home() {
 
-    let {theme} = useContext(ThemeContext)
+    let { theme } = useContext(ThemeContext)
 
-    return(
+    useEffect(() => {
+        const backAction = () => {
+            BackHandler.exitApp();
+            return True;
+        }
+
+        const backHandler = BackHandler.addEventListener(
+            'hardwareBackPress',
+            backAction,
+        );
+
+        return () => backHandler.remove();
+    }, []);
+
+    return (
         <View
             style={{
                 flex: 1,
                 backgroundColor: theme.background
             }}>
 
-                <MyHeader />
+            <MyHeader />
 
-                <MyList />
+            <MyList />
 
         </View>
     )
