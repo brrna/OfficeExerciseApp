fdd5abd35135a915141fa878785ef179c9f7dfb7
brrna <bern.varol@gmail.com>
2024-04-04 12:58:05 +0300

    gifs deleted

--- src/values/Gifs.js
+++ src/values/Gifs.js
@@ -1,23 +1,15 @@
 export const BackExercise = [
-    require("../assests/gifs/wall-pushup.gif"),
-    require("../assests/gifs/wall-sit.gif"),
-    require("../assests/gifs/sitting-spine-bending.gif")
+    require("../assests/gifs/wall-pushup.gif")
 ];
 
 export const LegExercise = [
-    require("../assests/gifs/side-lunges.gif"),
-    require("../assests/gifs/calf-raises.gif"),
-    require("../assests/gifs/leg-stretches.gif")
+    require("../assests/gifs/side-lunges.gif")
 ];
 
 export const NeckExercise = [
-    require("../assests/gifs/shoulder-circle.gif"),
-    require("../assests/gifs/neck-circle.gif"),
-    require("../assests/gifs/head-tilt.gif")
+    require("../assests/gifs/shoulder-circle.gif")
 ];
 
 export const WholeBodyExercise = [
-    require("../assests/gifs/pick-up-knees.gif"),
-    require("../assests/gifs/jumping-jack.gif"),
-    require("../assests/gifs/body-stretch.gif")
+    require("../assests/gifs/pick-up-knees.gif")
 ];
